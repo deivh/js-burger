74b91a2f1af7af4e8c3f0981b6e0f8f90eb1eb39
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-03-11 16:06:04 +0100

    js: added validation on burger name

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 var generateBtn = document.getElementById('generate');
+generateBtn.addEventListener ('click', function(){
+    console.log("Yay you clicked me!");
+
+    var burgerName = document.getElementById('burger-name').value;
+    if (!burgerName) {
+        alert('Inserisci il nome del panino e riprova');
+    } else {
+        console.log("Hai inserito un nome valido");
+        var check = document.getElementsByClassName('recipe');
+        console.log(check);
+    }
+})
