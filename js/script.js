de411e7b517b949d4b3e1812827d1aef71e1031f
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-03-11 16:33:32 +0100

    js: complete; no details

--- js/script.js
+++ js/script.js
@@ -9,5 +9,32 @@ generateBtn.addEventListener ('click', function(){
         console.log("Hai inserito un nome valido");
         var check = document.getElementsByClassName('recipe');
         console.log(check);
+        var priceTotal = 10;
+        for (var i=0; i<check.length; i++) {
+            var checkFor = check[i];
+            var checkTrue = checkFor.checked;
+            var priceAdd = parseInt(checkFor.dataset.price);
+            console.log(checkFor, checkTrue, priceAdd);
+            priceTotal += priceAdd;
+        }
+        console.log(priceTotal);
+        var coupon = ['BU0N01LP4N1N0', 'BU0N01LP4N1N2', 'BU0N01LP4N1N3', 'BU0N01LP4N1N4']
+        var couponTrue = document.getElementById('burger-coupon').value;
+        console.log(couponTrue);
+        if (!couponTrue) {
+            document.getElementById('price').innerHTML = priceTotal;
+        } else if (couponTrue) {
+            for (var i=0; i<coupon.length; i++) {
+                if (couponTrue == coupon[i]) {
+                    priceTotal = (priceTotal / 100) * 80;
+                    break;
+                } else {
+                    console.log("Il coupon inserito non è valido");
+                }
+            }
+        }
+        document.getElementById('price').innerHTML = priceTotal;
     }
+
+
 })
